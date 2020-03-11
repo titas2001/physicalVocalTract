1e3f7bd4bb4c3f653f08b52bcda3c30f96b831f6
Riccardo Russo <russo.riccardo2@gmail.com>
2020-03-11 17:14:50 +0100

    Added damping, fixed explosion with excitation
    Added damping dependent from 1 time derivative, implemented both with center and backwards derivative. Center derivative seems to need a very big damping factor to work properly. For backwards something weird happens with beta >= 3.427
    
    Excitation needs to be applied to u(2) and NOT to u(1) as the latter is never updated

--- Eq1D.m
+++ Eq1D.m
@@ -3,63 +3,85 @@ close all;
 
 fs = 44100;         % sample rate
 k = 1 / fs;         % time step
-dur = fs;           % duration
+dur = 3*fs;           % duration
 
-% Define variables of the system
+% Define speed of sound
 c = 344;
 
 % Calculate grid spacing from variables
 h = c * k;
-N = floor(1/h);
-%h = 3 / N;
+N = floor(1/h); %length of the tube
+%h = 1 / N;
 
 % Calculate courant number
 lambdaSq = c^2 * k^2 / h^2
 
-% Initialise u^{n+1} and u^n
+%Damp coefficient
+%(something weird happens if beta>=3.427, investigate)
+beta = 2;
+
+% Initialise spatial states u(n+1) and u(n)
 uNext = zeros(N, 1);
 u = zeros(N, 1);
-u(2) = 1; %exiciting the first samples
-u(1) = 0; 
-% initialise u^{n-1}
+
+% Exiciting with impulse at closed end
+% (Impulse response)
+%u(2) = 1; 
+
+% Initialise spatial state u(n-1)
 uPrev = u;
 
-% Initialise output and output position
+% Initialise output vector
 out = zeros(dur, 1);
+
+% Defining where output is observed, in our case the end of the tube
 outPos = N;
 
-%% Tube Function
-
-S = ones(N,1);
-D = N - floor(N/4);
-for i = D:N
-    S(i) = exp((i-D)*0.04);
-end
-%plot(S)
-
+% Initialising excitation function
 exciter = zeros(1,dur);
 
 for n = 1:dur 
-     %Exciter
-     exciter(n) = sawtooth(2*pi*20*(n-1)/fs);
-     %u(1) = exciter(n);
+     %Exciter processing
+     exciter(n) = sawtooth(2*pi*80*(n-1)/fs);
+     u(2) = exciter(n);
      
+     %Wave processing damp backwards derivative
      for l = 2:N
          if l == N %Free end
-            uNext(l) = 2 * u(l) - uPrev(l) + lambdaSq * (2 * u(l-1) - 2 * u(l)); 
+            uNext(l) = (2-beta*k) * u(l) + (beta*k-1) * uPrev(l) + lambdaSq * (2 * u(l-1) - 2 * u(l)); 
          else
-            uNext(l) = 2 * u(l) - uPrev(l) + lambdaSq * (u(l+1) - 2 * u(l) + u(l-1));
+            uNext(l) = (2-beta*k) * u(l) + (beta*k-1) * uPrev(l) + lambdaSq * (u(l+1) - 2 * u(l) + u(l-1));
          end
      end
-    % retrieve output
+     
+%      %Wave processing damp center derivative
+%      for l = 2:N
+%          if l == N %Free end
+%             uNext(l) = (4/(2+beta*k)) * u(l) + (beta*beta*k*k/4 - 1) * uPrev(l) + (2*lambdaSq/(2+beta*k)) * (2 * u(l-1) - 2 * u(l)); 
+%          else
+%             uNext(l) = (4/(2+beta*k)) * u(l) + (beta*beta*k*k/4 - 1) * uPrev(l) + (2*lambdaSq/(2+beta*k)) * (u(l+1) - 2 * u(l) + u(l-1));
+%          end
+%      end
+
+%      %Wave processing no damp
+%      for l = 2:N
+%          if l == N %Free end
+%             uNext(l) = 2 * u(l) - uPrev(l) + lambdaSq * (2 * u(l-1) - 2 * u(l)); 
+%          else
+%             uNext(l) = 2 * u(l) - uPrev(l) + lambdaSq * (u(l+1) - 2 * u(l) + u(l-1));
+%          end
+%      end
+     
+    % Retrieve output, filling output vector
     out(n) = uNext(outPos);
     
-    % draw string
-    %plot(uNext);
-    %plot(exiciter);
-    %ylim([-1, 1]);
-    %drawnow;
+%     % Real time states drawing
+%     plot(uNext);
+%     %plot(exiciter);
+%     ylim([-1, 1]);
+%     drawnow;
     
+    % Update spatial states
     uPrev = u;
     u = uNext;
 end
