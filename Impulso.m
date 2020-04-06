386af1703a47cfeba27e72d27cd2bcd19619d636
Francisco <franciscorreia98@gmail.com>
2020-04-06 15:59:19 +0200

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- Impulso.m
+++ Impulso.m
@@ -48,5 +48,23 @@ switch mode
     case 'sawtooth'
         
         imp = sawtooth(2*pi*freq*t/fs);
+        
+    case 'camel'
+        
+        t = 0.001:0.001:1;
+        e1 = 0.1;
+        e2 = 0.4;
+        
+        for i=1:length(t)
+            if t(i)<e1
+               x(i) = 0.5-0.5*cos(2*pi*freq*t(i));
+            elseif t(i)<=e2
+               x(i) = -((0.5-0.5*cos(2*pi*freq*e1))/(e2-e1))*((t(i)-e2));
+            else
+               x(i) = 0;
+            end
+        end
+         xx = repmat(x, [1, time]);
+         imp = xx;
 
 end
